a0625db90c03fc73400591625be89d69827979d8
michaelhla <michaelhla@college.harvard.edu>
2025-02-06 00:07:45 +0000

    mpnn full

--- evals/mpnn.py
+++ evals/mpnn.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
 # Install required packages
 import subprocess
-subprocess.run(["pip", "install", "git+https://github.com/dauparas/ProteinMPNN.git"], check=True)
+subprocess.run(["pip", "-q", "install", "git+https://github.com/sokrypton/ColabDesign.git@v1.1.1"], check=True)
 
 import json
 from pathlib import Path
@@ -13,31 +13,40 @@ from tqdm import tqdm
 from colabdesign.mpnn import mk_mpnn_model
 from stability_reward import StabilityRewardCalculator
 
+# Results summary:
+# Number of enzymes processed: 40
+# Number of successful improvements: 29
+# Success rate: 72.5%
+# Max stability improvement: -2151.464
+# Average percentage of mutations between original and mutated sequences: 54.1% (when free flowing mpnn)
+
 # Initialize models
 mpnn_model = mk_mpnn_model()
 stability_calculator = StabilityRewardCalculator()
 
 def main():
     # Load previous results
-    with open('results/selected_enzymes.json', 'r') as f:
+    with open('results/mpnn_stability_mutations.json', 'r') as f:
         selected_enzymes = json.load(f)
     
     results = []
     
     for enzyme_id, data in tqdm(selected_enzymes.items(), desc="Processing enzymes"):
         sequence = data['sequence']
-        
+                
         try:
             # Calculate original stability
-            original_stability = stability_calculator.calculate_stability(sequence)
+            if data['original_stability'] is not None:
+                original_stability = data['original_stability']
+            else:
+                original_stability = stability_calculator.calculate_stability(sequence)
             
             # Get structure prediction for MPNN
             pdb_file = stability_calculator.predict_structure(sequence)
             
             # Generate new sequence with MPNN
             mpnn_model.prep_inputs(pdb_filename=pdb_file)
-            mutated_sequence = mpnn_model.sample()
-            
+            mutated_sequence = mpnn_model.sample()['seq'][0]            
             # Calculate new stability
             new_stability = stability_calculator.calculate_stability(mutated_sequence)
             
